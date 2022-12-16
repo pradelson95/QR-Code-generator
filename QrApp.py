2e6ef181a657027e1f74bf1ecb7190410df2c958
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2022-12-15 18:27:11 -0800

    Comente parte del codigo
    Comente parte del codigo para mejor lectura y futuras mejoras

--- QrApp.py
+++ QrApp.py
@@ -1,43 +1,46 @@
+# Importamos las librerías necesarias
 from tkinter import *
 from tkinter import messagebox
 import qrcode
 import os
 
+# Creamos la ventana principal de la aplicación con Tkinter
 window = Tk()
-window.title("My App")
-window.geometry("1000x700")
-window.configure(background="gray")
-window.resizable(False, False)
+window.title("My App")  # Establecemos el título de la ventana
+window.geometry("1000x700")  # Establecemos el tamaño de la ventana
+window.configure(background="gray")  # Establecemos el color de fondo de la ventana
+window.resizable(False, False)  # Deshabilitamos la opción de redimensionar la ventana
 
-
+# Creamos una función para eliminar el texto de placeholdet al hacer clic en el campo de nombre de la imagen del código QR
 def temp_text_2(e):
     ImageQrCodeName.delete(0, END)
 
-
+# Creamos una función para eliminar el texto de placeholdet al hacer clic en el campo de enlace del código QR
 def temp_text(e):
     QrCodelink.delete(0, END)
 
-
+# Creamos una función para generar y guardar el código QR cuando se hace clic en el botón
 def save_qr_code_img():
     global ImageQrCodeName
-    link = QrCodelink.get()
-    save_img_as = ImageQrCodeName.get()
+    link = QrCodelink.get()  # Obtenemos el enlace o mensaje de texto ingresado por el usuario
+    save_img_as = ImageQrCodeName.get()  # Obtenemos el nombre de la imagen ingresado por el usuario
 
-    # mostrar un mensaje de error si el usuario deja los ambos campos vacios
+    # Si ambos campos están vacíos, mostramos un mensaje de error
     if link == "" and ImageQrCodeName == "":
         messagebox.showerror("Error", "The link field is empty")
-    # crear el codigo QR si los campos estan llenos
+    # Si ambos campos están llenos, creamos el código QR
     else:
+        # Creamos un objeto QR con qrcode y le asignamos algunas propiedades
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
             box_size=10,
             border=4
         )
-        qr.add_data(link)
-        qr.make(fit=True)
-        set_image_property = qr.make_image(fill_color="black", back_color="red")
-        s = set_image_property.save(save_img_as + ".png")
+        qr.add_data(link)  # Añadimos el enlace o mensaje de texto al objeto QR
+        qr.make(fit=True)  # Creamos el código QR
+        set_image_property = qr.make_image(fill_color="black", back_color="red")  # Establecemos algunas propiedades de la imagen
+        s = set_image_property.save(save_img_as + ".png") 
 
         # poner un texto en la interfaz que le dice al usuario donde esta ubicado la imagen del codigo QR
         Imgpath = Label(window, text=os.path.abspath(save_img_as + ".png"), font=("Arial", 22, "bold"), bg="gray",
